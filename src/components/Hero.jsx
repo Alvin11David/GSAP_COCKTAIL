a7f8a93a6eecd3ebb1829011c84a733fb50051c9
Alvin11David <alvin69david@gmail.com>
2025-08-22 15:01:09 +0300

    Added the p element

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,28 @@ const Hero = () => {
         <section id="hero" className="noisy">
             <h1 className="title">MOJITO</h1>
             <img
-            src="/images/hero-left-leaf.png"
-            alt="leaf-image"
-            className="left-leaf"/>
+                src="/images/hero-left-leaf.png"
+                alt="leaf-image"
+                className="left-leaf"/>
+            <img
+                src="/images/hero-right-leaf.png"
+                alt="leaf-image"
+                className="right-leaf"/>
+            <div className="body">
+                <div  className="content">
+                    <div className="space-y-5 hidden md:block">
+                        <p>Cool. Crispy. Classic</p>
+                        <p className="subtitle">
+                            Sip the Spirit  <br />of Summer
+                        </p>
+                    </div>
+                    <div className='view-cocktails'>
+                        <p className='subtitle'>
+                            Every Cocktail on our Menu is a blend of premium indgredients, creative flair, and timeless recipes - designed to delight your senses.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 };
