6925a5fea31c3af17992e8a735ef5c9bce587866
Alvin11David <alvin69david@gmail.com>
2025-08-25 15:20:57 +0300

    Added the title gsap animation

---
+++ src/components/cocktails.jsx
@@ -0,0 +1,10 @@
+const Cocktails = () => {
+    return (
+        <section id='cocktails' className="noisy">
+            <img src="/images/cocktail-left-leaf.png" id="c-left-leaf" alt="leaf-image" className="c-left-leaf"/>
+            <img src="/images/cocktail-right-leaf.png" id="c-right-leaf" alt="leaf-image" className="c-right-leaf"/>
+        </section>
+    );
+};
+
+export default Cocktails;
